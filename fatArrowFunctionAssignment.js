a18645fcfc40a509164c982dc05c18ee0ae1e072
Sonal <mahendra.sonal@gmail.com>
2017-12-25 20:02:07 +0530

    FrontEndDevelopmentAssignment2
    Fat Arrow Function Assignment

---
+++ fatArrowFunctionAssignment.js
@@ -0,0 +1,5 @@
+var setNameId = function (name, id) {
+    return ("hi your name is " + name + " id is " + id);
+};
+var test = setNameId("sonal", 1);
+console.log(test);
---
+++ fatArrowFunctionAssignment.ts
@@ -0,0 +1,10 @@
+
+let setNameId=(name:string,id:number)=>
+{
+    return ("hi your name is "+name+" id is "+id);
+
+}
+//let test=setNameId("sonal",1);
+ //console.log(test);
+
+     
